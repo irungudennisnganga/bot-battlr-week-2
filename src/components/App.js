92dc4e0af793d238a16418d1cfd02fefd42abb54
+dennisngangairungu <+>
2023-11-18 08:17:20 +0300

    added a components folder to hold the components:

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../components/App.css';
+import BotCollection from './BotCollection';
+function App() {
+  return (
+    <div className="App">
+      <BotCollection />
+    </div>
+  );
+}
+
+export default App;
