88065d6817bb253e18b03149d38cf839f01d6af3
+dennisngangairungu <+>
2023-11-18 19:55:22 +0300

    rendering the bots

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import '../components/App.css';
 import BotCollection from './BotCollection';
+import { useState, useEffect } from 'react';
+
+
 function App() {
+  //takes the initial data when making fetch request
+  const [data, setData] = useState([])
+
+useEffect( () => {
+  fetch(`http://localhost:8001/bots`)
+    .then(r => r.json())
+    .then(bots => setData(bots))
+
+
+},[] )
+
+//console.log(data)
   return (
     <div className="App">
-      <BotCollection />
+      <BotCollection data={data} />
     </div>
   );
 }
--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,11 +1,71 @@
 import React from 'react'
+import "./BotCollection.css"
 
-function BotCollection() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+function BotCollection({data}) {
+console.log(data)
+
+function handleClick(){
+    console.log("Clicked")
 }
+  return (
+    <div   id='main-card' >
+        
+        {data.map( bot => {
+         return (
+            <div onClick={handleClick} key={bot.id} className="card-wrapper">
+            <div className="card">
+              <div className="image-wrapper">
+               
+             <img src={bot.avatar_url} alt='bot' />
+               
+            </div>
+              <div className="text-box-wrapper">
+              <div className="text-box">
+                <h3 className="heading">
+                Name : {bot.name}
+               </h3>
+                <p  className="text">
+                Catch phrase : {bot.catchphrase}
+               </p>
+              <p className="text">
+                   Health : {bot.health}
+              </p>
+              <p className="text">
+                 Damage : {bot.damage}
+              </p> 
+              <p className="text">
+                   Class : {bot.bot_class}
+              </p> 
+              <p className="text"> 
+                Last update : {bot.updated_at}
+              </p>  
+             </div>
+            </div>
+             
+       
+           </div>
+        </div>
+
+
+         )})}
+    </div>
+
+
+                  
+           
+            )}
+  
+
 
 export default BotCollection
+
+{/* <div id='card' key={bot.id} >
+                 <img src={bot.avatar_url} alt='bot'/>
+                 <h3>Name : {bot.name}</h3>
+                 <p>Catch phrase : {bot.catchphrase}</p>
+                 <p>Health : {bot.health}</p>
+                 <p>Damage : {bot.damage}</p>
+                 <p>Class : {bot.bot_class}</p>
+                 <p>Last update : {bot.updated_at}</p>
+            </div> */}
